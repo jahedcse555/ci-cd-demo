975c2fe87952e1e984f546082f9b4dbceb4e0cd8
Jahed Ahmed <38354860+jahedcse555@users.noreply.github.com>
2025-08-30 15:37:52 +0200

    add jahed

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello CI/CD World!");
+  res.send("Hello Jahed's CI/CD World!");
 });
 
 app.listen(3000, () => console.log("Server running on port 3000"));
