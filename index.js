56301fda983888860aed17441b292221eb204abf
Jahed Ahmed <38354860+jahedcse555@users.noreply.github.com>
2025-09-01 10:15:14 +0200

    Internal Server Error

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const session = require("express-session");
 const multer = require("multer");
 const path = require("path");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 
 const app = express();
 app.set("view engine", "ejs");
